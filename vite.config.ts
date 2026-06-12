22229240443598883a80bdd659458e072816a5d1
josevagula <jose.gomes.nm.2023@gmail.com>
2026-06-12 20:12:57 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
-retornar {
-  base: '/',
+exportar padrão defineConfig(() => {
+  return {
+    base:'/',
   plugins:[reagir(), tailwindcss()],
     resolve: {
       alias: {
