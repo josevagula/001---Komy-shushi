641c364cba8e195e4eb85eccfca987896c0223de
josevagula <jose.gomes.nm.2023@gmail.com>
2026-06-28 15:18:59 -0300

    feat: update Hot Aberto and Hot Crispy images to jpg

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,8 @@ export default function App() {
     const imageFix: Record<string, string> = {
       'hot-1': '/images/Hot Holl.jpg',
       'hot-2': '/images/Hot Pimenta Bico.jpg',
+      'hot-3': '/images/Hot Crispy.jpg',
+      'hot-5': '/images/Hot Aberto.jpg',
       'hot-6': '/images/Hot Temaki.jpg',
       'hot-7': '/images/Hot Temaki sem Arroz.jpg',
       'temaki-2': '/images/Hot Temaki sem Arroz.jpg',
